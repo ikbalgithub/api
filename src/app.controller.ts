8ccc42d10b49afdfb3bed788bd413e3117706e8e
mi379 <mkzbdfgh@gmail.com>
2024-02-09 09:56:44 +0700

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller,Get,UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from './guards/auth.guard'
 
-@Controller() export class AppController {
-  @Get() getMotherFucker():string{
+@Controller('app') export class AppController {
+  @Get('') getMotherFucker():string{
     return 'hello motherfucker'
   }
 }
