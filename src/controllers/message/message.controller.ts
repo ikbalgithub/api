f08b5372f072694dcd2a60fdd78f4cebd6d7de45
mkzbdfgh <mkzbdfgh@gmail.com>
2024-06-12 12:29:30 +0000

    ...

--- src/controllers/message/message.controller.ts
+++ src/controllers/message/message.controller.ts
@@ -94,9 +94,9 @@ import { Profile } from 'src/schemas/profile.schema'
       this.rabbitMq.send(`messages/${dto.accept}`,`history/newMessage~history/${dto.accept}~${JSON.stringify(result)}`) // works
       this.rabbitMq.send(`messages/${dto.accept}`,`history/message~history/${dto.accept}~${JSON.stringify(populated)}`)
      
-      //this.rabbitMq.send(`detail/${dto.accept}`,`history/newMessage-history/${dto.accept}-${JSON.stringify(result)}`)
-      //this.rabbitMq.send(`detail/${dto.accept}`,`history/message-history/${dto.accept}-${JSON.stringify(populateds)}`)
-      //this.rabbitMq.send(`detail/${dto.accept}`,`chat/${dto.accept}/${sender}-incomingMessage-${JSON.stringify(result)}`)
+      this.rabbitMq.send(`detail/${dto.accept}`,`history/newMessage-history/${dto.accept}-${JSON.stringify(result)}`)
+      this.rabbitMq.send(`detail/${dto.accept}`,`history/message-history/${dto.accept}-${JSON.stringify(populated)}`)
+      this.rabbitMq.send(`detail/${dto.accept}`,`chat/${dto.accept}/${sender}-incomingMessage-${JSON.stringify(result)}`)
 
       //this.gateway.newMessage<Message>(result,[`history/${dto.accept}`,`chat/${dto.accept}/${sender}`]) // only message
       //this.gateway.message<Omit<Last_Message,"unreadCounter">>(populated,[`history/${dto.accept}`]) // populated message
