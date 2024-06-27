f8bfbae028039232ca981936b2f49f73f2e9ee5f
mkzbdfgh <mkzbdfgh@gmail.com>
2024-06-27 21:49:30 +0000

    ...

--- src/gateways/events/events.gateway.ts
+++ src/gateways/events/events.gateway.ts
@@ -41,12 +41,10 @@ import { Inject } from '@nestjs/common';
     var channels = this.rabbitMq.channels.filter(
       c => c.id === socket.id
     )
-    
-    channels.forEach(channel => {
-      this.rabbitMq.channels = this.rabbitMq.channels.filter(
-        c => c.id !== socket.id
-      )
-    })
+
+    this.rabbitMq.channel = this.rabbitMq.channels.filter(
+      c => c.id !== socket.id
+    )
 
     channels.forEach(c => {
       c.channel?.close()
