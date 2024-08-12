2f28a09ed24e1254689c551da07d677fe80e3970
mkzbdfgh <mkzbdfgh@gmail.com>
2024-08-12 09:13:44 +0000

    development

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { ProfileService } from './services/profile/profile.service';
 import { OauthController } from './controllers/oauth/oauth.controller';
 import { ProfileController } from './controllers/profile/profile.controller';
 import { RabbitmqService } from './services/rabbitmq/rabbitmq/rabbitmq.service';
+import { RabbitmqController } from './controllers/rabbitmq/rabbitmq.controller';
 
 @Module({
   imports: [
@@ -50,7 +51,8 @@ import { RabbitmqService } from './services/rabbitmq/rabbitmq/rabbitmq.service';
     UserController,
     MessageController,
     OauthController,
-    ProfileController
+    ProfileController,
+    RabbitmqController
   ],
   providers: [
     UserService,
