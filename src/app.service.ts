e78c4551ad8970f937ae2e4073125fb495d6fbce
mkzbdfgh <mkzbdfgh@gmail.com>
2024-08-13 23:59:10 +0000

    development

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { EventPattern,Payload } from '@nestjs/microservices';
+import { MessagePattern,Payload } from '@nestjs/microservices';
 
 @Injectable() export class AppService {
-  @EventPattern('microservice') onMessage(@Payload() payload:any){
+  @MessagePattern('microservice') onMessage(@Payload() payload:any){
     console.log(payload)
   }
 }
