b3eb344c10ec91a39524c84e0e43c1104846bedd
kogebngadak <kogebngadak@gmail.com>
2024-09-07 13:47:05 +0000

    development

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -5,20 +5,6 @@ import { UserService } from './user.service';
 @Resolver(of => User) export class UserResolver {
   constructor(private service:UserService) {}
 
-  @Query(r => User) async findByUsername(){
-    try{
-      var result = await this.service.findByUsername(
-        'u1'
-      )
-
-      return result
-    }
-    catch(e:any){
-      console.log(e.message)
-      return e.message
-    }
-  }
-
   @Query(returns => User) async example(){
     return {
       id:'1234'
