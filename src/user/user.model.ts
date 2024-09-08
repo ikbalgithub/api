3234fca316b99c7825980b0bacb57f2ca8dcc4b4
kogebngadak <kogebngadak@gmail.com>
2024-09-08 03:45:25 +0000

    development

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -2,6 +2,9 @@ import { Types } from 'mongoose'
 import { Field, ID, ObjectType, } from '@nestjs/graphql';
 
 @ObjectType() export class User {
+  @Field(r => ID)
+  _id: Types.ObjectId
+
   @Field({nullable:true})
   oauthReference:string
   
