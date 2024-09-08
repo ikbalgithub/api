b8b4f19ca60a7abff2f99c1bd5acf64bc323b12d
kogebngadak <kogebngadak@gmail.com>
2024-09-08 08:23:27 +0000

    development

--- src/profile/profile.resolver.ts
+++ src/profile/profile.resolver.ts
@@ -1,9 +1,9 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Profile } from './profile.model';
-import { UserService } from './profile.service';
+import { ProfileService } from './profile.service';
 import { GraphQLError } from 'graphql';
 
 @Resolver() export class ProfileResolver {
-  constructor(private readonly profileService:UserService) {}
+  constructor(private readonly profileService:ProfileService) {}
 
 }
