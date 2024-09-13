d370b4184ccabf1f1a4f4b7527d512791ce38b8c
kogebngadak <kogebngadak@gmail.com>
2024-09-13 06:53:21 +0000

    development

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,20 @@ import { Transport,MicroserviceOptions } from '@nestjs/microservices';
 (async function(){
 
   const app = await NestFactory.create(
-    AppModule,{cors:false}
+    AppModule
   )
 
-  // app.enableCors(
-  //   {
-  //     credentials:true,
-  //     origin: [
-  //       'https://api-production-bdf9.up.railway.app',
-  //       'https://3000-idx-api-1724452153719.cluster-mwrgkbggpvbq6tvtviraw2knqg.cloudworkstations.dev/graphql',
-  //       'https://4200-idx-messenger-1724914463934.cluster-e3wv6awer5h7kvayyfoein2u4a.cloudworkstations.dev',
-  //       'https://ngx-messenger.vercel.app'
-  //     ],
-  //   }
-  // )
+  app.enableCors(
+    {
+      credentials:true,
+      origin: [
+        'https://api-production-bdf9.up.railway.app',
+        'https://3000-idx-api-1724452153719.cluster-mwrgkbggpvbq6tvtviraw2knqg.cloudworkstations.dev/graphql',
+        'https://4200-idx-messenger-1724914463934.cluster-e3wv6awer5h7kvayyfoein2u4a.cloudworkstations.dev',
+        'https://ngx-messenger.vercel.app'
+      ],
+    }
+  )
 
   app.useGlobalPipes(
     new ValidationPipe({
